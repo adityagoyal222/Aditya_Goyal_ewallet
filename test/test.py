351a20afdf6aac288be2beb797b87360ea921798
Aditya Goyal <43295337+adityagoyal222@users.noreply.github.com>
2020-09-04 12:52:17 +0545

    updated comments

--- test/test.py
+++ test/test.py
@@ -8,28 +8,28 @@ import unittest
 
 class UnitTesting(unittest.TestCase):
 	"""Unit Testing class"""
-	def test_login(self):  # run this test after test_signup.
+	def test_login(self):
 		login = Login()
 		actual_result = login.login_user('tester', 'tester')
 		self.assertTrue(actual_result)
 
-	def test_service(self):  # run this test after test_service
+	def test_service(self):
 		service = Service()
 		actual_result = type(service.get_service_name())
 		expected_result = type([])
 		self.assertEqual(actual_result, expected_result)
 
-	def test_load_amount(self):  # run this test after test_service
+	def test_load_amount(self):
 		account = Account()
 		actual_result = account.load_amount('tester', 'Janata Bank', '1234', 30000)
 		self.assertTrue(actual_result)
 
-	def test_pay_amount(self):  # run this test after test_load_amount
+	def test_pay_amount(self):
 		account = Account()
 		actual_result = account.pay_amount('tester', 'Nepal Electricity Board', 500)
 		self.assertTrue(actual_result)
 
-	def test_show_transaction(self):  # run this test after test_pay_amount
+	def test_show_transaction(self):
 		account = Account()
 		actual_result = type(account.show_transaction('tester'))
 		expected_result = type([])
